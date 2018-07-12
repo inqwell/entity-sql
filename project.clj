51e9a9a3c2349f538c2a6cf321454987562f791e
Tom Sanders <tom.sanders@inqwell.com>
2018-07-12 21:24:34 +0100

    Release 0.1.2 - update dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject entity/entity-sql "0.1.1"
+(defproject entity/entity-sql "0.1.2"
   :description "SQL persistence for use with entity-core"
   :url "https://github.com/inqwell/entity-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[entity/entity-core "0.1.0"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/java.jdbc "0.6.1"]
-                 [com.layerware/hugsql "0.4.7"]
-                 [hikari-cp "1.7.5"]]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [com.layerware/hugsql "0.4.8"]
+                 [hikari-cp "1.8.3"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox {:exclude-vars #"trail-.*|trunc-.*|^(map)?->\p{Upper}"
           :output-path "codox/entity-sql"
