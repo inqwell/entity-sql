4330a2121f9969b260c9b6d7d8089da29b06f7cc
Tom Sanders <tom.sanders@inqwell.com>
2019-12-28 14:53:21 +0000

    entty/core 0.1.2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/inqwell/entity-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[entity/entity-core "0.1.0"]
+  :dependencies [[entity/entity-core "0.1.2"]
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/java.jdbc "0.7.5"]
                  [com.layerware/hugsql "0.4.8"]
